91138bf5465587426dc6c3a20f84f2143cdaea0b
Govinda B <govinda.b@thinkbridge.in>
2022-02-17 00:19:42 +0530

    2726,2728 Abstraction  and Polymorphism(Method Overloading)

---
+++ OOP/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace OOP
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle()
+        {
+        }
+        public double Area(double radius)
+        {
+            return PI * radius * radius;
+        }
+
+        public override double Area()
+        {
+            return PI * Radius * Radius;
+        }
+    }
+}
