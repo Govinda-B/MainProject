5975c03c08bf4a9737a9244aa105b749a4abf1e8
Govinda B <govinda.b@thinkbridge.in>
2022-02-17 00:19:42 +0530

    2726,2728 Abstraction  and Polymorphism(Method Overloading)

---
+++ OOP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            double area = c1.Area(22);
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ OOP/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Square : Shape
+    {
+        double side;
+        public Square(double side)
+        {
+            this.side = side;
+        }
+        public override double Area()
+        {
+            return side * side;
+        }
+    }
+}
