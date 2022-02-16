15f296b56bc3097c96a171afa19b452e5290e5bb
Govinda B <govinda.b@thinkbridge.in>
2022-02-16 20:14:12 +0530

    2723 Classes and Objects Creation of class and objects

---
+++ OOPConcepts/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConcepts
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public void Stock(int quantity)
+        {
+            Quantity += quantity;
+        }
+        public int Buy(int quantity)
+        {
+            if (quantity>Quantity)
+            {
+                return 0;
+            } 
+            Quantity -= quantity;
+            return quantity * Price;
+        }
+    }
+}
---
+++ OOPConcepts/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOPConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product cookie = new Product
+            {
+                Id = 1,
+                Name = "Cookie",
+                Quantity = 100,
+                Price = 5
+            };
+            Product product1 = new Product
+            {
+                Id = 1,
+                Name = "Cookie",
+                Quantity = 100,
+                Price = 5
+            };
+            Product product2 = new Product
+            {
+                Id = 2,
+                Name = "Chocolate",
+                Quantity = 100,
+                Price = 15
+            };
+            Product product3 = new Product
+            {
+                Id = 3,
+                Name = "Milk Chocolate",
+                Quantity = 100,
+                Price = 10
+            };
+
+            int Cost = product1.Buy(10);
+            Console.WriteLine(Cost);
+        }
+    }
+}
