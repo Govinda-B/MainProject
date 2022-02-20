599d70cae81261cd4a3c8a066d9e60cb0482a716
Govinda B <govinda.b@thinkbridge.in>
2022-02-21 01:36:36 +0530

    Added SortedList and Dictionary

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -32,6 +32,37 @@ namespace Collection
             Console.WriteLine(name.Pop());
             Console.WriteLine(name.Pop());
 
+
+            SortedList<string, string> openWith =
+            new SortedList<string, string>();
+
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+
+            foreach (KeyValuePair<string, string> kvp in openWith)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
+
+            Console.WriteLine("For key = \"rtf\", value = {0}.",openWith["rtf"]);
+
+            Dictionary<string, string> keyValuePairs =new Dictionary<string, string>();
+            keyValuePairs.Add("a", "one");
+            keyValuePairs.Add("b", "two");
+            keyValuePairs.Add("c", "three");
+            keyValuePairs.Add("d", "four");
+            Console.WriteLine("For key = \"a\", value = {0}.", keyValuePairs["a"]);
+
+            foreach (KeyValuePair<string, string> kvp in keyValuePairs)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
+
+
         }
     }
 }
