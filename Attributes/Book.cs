fab561b4ed6b98dfa54ec3448facf199f251bc7c
Govinda B <govinda.b@thinkbridge.in>
2022-02-21 10:47:49 +0530

    Ussing In Out Attributes

--- Attributes/Book.cs
+++ Attributes/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,10 @@ namespace Attributes
                 addition += item;
             }
             return addition;
+        }
+        public static void Square([In,Out]ref int number)
+        {
+            number *= number;
         }
     }
 }
