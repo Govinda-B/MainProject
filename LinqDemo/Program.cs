74397ee326de36b1d9da478754c8a04457032dd2
Govinda B <govinda.b@thinkbridge.in>
2022-02-23 10:49:40 +0530

    Adding Queries methods

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -62,6 +62,70 @@ namespace LinqDemo
             {
                 Console.WriteLine(item);
             }
+
+            // Specify the data source.
+            int[] scores = { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            int[] nums = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // QueryMethod1 returns a query as the value of the method.
+            var myQuery1 = Queries.QueryMethod1(nums);
+
+            // Query myQuery1 is executed in the following foreach loop.
+            Console.WriteLine("Results of executing myQuery1:");
+            // Rest the mouse pointer over myQuery1 to see its type.
+            foreach (var s in myQuery1)
+            {
+                Console.WriteLine(s);
+            }
+
+            // You also can execute the query returned from QueryMethod1
+            // directly, without using myQuery1.
+            Console.WriteLine("\nResults of executing myQuery1 directly:");
+            // Rest the mouse pointer over the call to QueryMethod1 to see its
+            // return type.
+            foreach (var s in Queries.QueryMethod1(nums))
+            {
+                Console.WriteLine(s);
+            }
+
+            // QueryMethod2 returns a query as the value of its out parameter.
+            Queries.QueryMethod2(nums, out IEnumerable<string> myQuery2);
+
+            // Execute the returned query.
+            Console.WriteLine("\nResults of executing myQuery2:");
+            foreach (var s in myQuery2)
+            {
+                Console.WriteLine(s);
+            }
+
+            // You can modify a query by using query composition. In this case, the
+            // previous query object is used to create a new query object; this new object
+            // will return different results than the original query object.
+            myQuery1 =
+                from item in myQuery1
+                orderby item descending
+                select item;
+
+            // Execute the modified query.
+            Console.WriteLine("\nResults of executing modified myQuery1:");
+            foreach (var s in myQuery1)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
---
+++ LinqDemo/Queries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    public class Queries
+    {
+        public static IEnumerable<string> QueryMethod1(int[] ints)
+        {
+            return
+            from i in ints
+            where i > 4
+            select i.ToString();
+        }
+        
+            
+
+        // QueryMethod2 returns a query as the value of the out parameter returnQ
+        public static void QueryMethod2(int[] ints, out IEnumerable<string> returnQ)
+        {
+            returnQ =from i in ints
+                     where i < 4
+                     select i.ToString();
+        }
+            
+            
+
+    }
+}
