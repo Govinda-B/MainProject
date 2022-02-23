d91a53f1a6f9b45e238c0f2bf85807f37168e572
Govinda B <govinda.b@thinkbridge.in>
2022-02-23 12:36:09 +0530

    Nested Group Query

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -211,7 +211,7 @@ namespace LinqDemo
                 }
             }
 
-
+            
             Console.WriteLine("\n\n\nGroup By comparison ");
             var groupByHighAverageQuery =
             from student in students
@@ -228,6 +228,33 @@ namespace LinqDemo
                 foreach (var student in studentGroup)
                 {
                     Console.WriteLine($"\t{student.FirstName} {student.LastName}");
+                }
+            }
+
+
+            Console.WriteLine("\n\n\nNested Group Query");
+            var nestedGroupsQuery =
+            from student in students
+            group student by student.Year into newGroup1
+            from newGroup2 in (
+            from student in newGroup1
+            group student by student.LastName
+            )
+            group newGroup2 by newGroup1.Key;
+
+            // Three nested foreach loops are required to iterate
+            // over all elements of a grouped group. Hover the mouse
+            // cursor over the iteration variables to see their actual type.
+            foreach (var outerGroup in nestedGroupsQuery)
+            {
+                Console.WriteLine($"DataClass.Student Level = {outerGroup.Key}");
+                foreach (var innerGroup in outerGroup)
+                {
+                    Console.WriteLine($"\tNames that begin with: {innerGroup.Key}");
+                    foreach (var innerGroupElement in innerGroup)
+                    {
+                        Console.WriteLine($"\t\t{innerGroupElement.LastName} {innerGroupElement.FirstName}");
+                    }
                 }
             }
         }
