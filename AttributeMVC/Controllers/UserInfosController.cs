f824b6e9aacac0329c386776f436fdfbedfbd0a9
Govinda B <govinda.b@thinkbridge.in>
2022-02-24 22:40:08 +0530

    Used Various Attributes and Also custom attribute email domain validation

---
+++ AttributeMVC/Controllers/UserInfosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AttributeMVC.Models;
+
+namespace AttributeMVC.Controllers
+{
+    public class UserInfosController : Controller
+    {
+        private readonly RepoContext _context;
+
+        public UserInfosController(RepoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserInfos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.userInfos.ToListAsync());
+        }
+
+        // GET: UserInfos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.userInfos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: UserInfos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserInfos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Email,Name")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userInfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.userInfos.FindAsync(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,Name")] UserInfo userInfo)
+        {
+            if (id != userInfo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userInfo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserInfoExists(userInfo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.userInfos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // POST: UserInfos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userInfo = await _context.userInfos.FindAsync(id);
+            _context.userInfos.Remove(userInfo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserInfoExists(int id)
+        {
+            return _context.userInfos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ AttributeMVC/Models/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttributeMVC.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [EmailValidation(allowedDomain: "thinkbridge.in", ErrorMessage = "The input mail id must be 'thinkbridge.in")]
+        public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
