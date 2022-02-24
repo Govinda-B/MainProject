83f4286a5df3a789539476903d457ac2e1a251fe
Govinda B <govinda.b@thinkbridge.in>
2022-02-24 22:40:08 +0530

    Used Various Attributes and Also custom attribute email domain validation

---
+++ AttributeMVC/Models/RepoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AttributeMVC.Models
+{
+    public class RepoContext:DbContext
+    {
+        public RepoContext(DbContextOptions<RepoContext> options):base(options)
+        {
+
+        }
+        public DbSet<UserInfo> userInfos { get; set; }
+    }
+}
