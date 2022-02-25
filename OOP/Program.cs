da8d16ac8b142a4968519b02b32d7a590b2aa8f6
Govinda B <govinda.b@thinkbridge.in>
2022-02-25 11:09:51 +0530

    Multiple Inheritance

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -7,8 +7,25 @@ namespace OOP
         static void Main(string[] args)
         {
             Circle c1 = new Circle();
-            double area = c1.Area(22);
+            double area = Circle.Area(22);
             Console.WriteLine(area);
+
+            //Multiple Inheritance
+            Child child = new Child(2,2.5f);
+            Console.WriteLine(child.Cube(3));
+            
+            Console.WriteLine(child.Cube());
+
+            Console.WriteLine(child.Cube(3.3));
+
+
+            Parent1 parent1 = new Parent1();
+            Parent2 parent2 = new Parent2();
+            Child2 child2 = new Child2(parent1,parent2);
+
+            Console.WriteLine(child2.Cube(2));
+            Console.WriteLine(child2.Cube(1.6f));
+            Console.WriteLine(child2.Cube());
         }
     }
 }
