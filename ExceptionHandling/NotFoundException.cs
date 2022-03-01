3dfd7006b771360ab57ce9f3d2774d71dfb193a6
Govinda B <govinda.b@thinkbridge.in>
2022-03-01 23:25:17 +0530

    Exception Handling Custom Exceptions

---
+++ ExceptionHandling/NotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class NotFoundException:Exception
+    {
+        public NotFoundException(string message):base(message)
+        {
+        }
+    }
+    //public class FileNotFoundException : NotFoundException
+    //{
+    //    public FileNotFoundException(string message) : base(message)
+    //    {
+    //    }
+    //}
+    //public class DirectoryNotFoundException : NotFoundException
+    //{
+    //    public DirectoryNotFoundException(string message) : base(message)
+    //    {
+    //    }
+    //}
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 98, b = 0;
+            double result;
+
+            try
+            {
+                result = SafeDivision.SafeDiv(a, b);
+                Console.WriteLine("{0} divided by {1} = {2}", a, b, result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Attempted divide by zero.");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Done with execution");
+            }
+            Console.WriteLine("\n\n\n");
+            try
+            {
+                throw new NotFoundException("File Not Found");
+            }
+            catch (NotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Done with execution");
+            }
+            Console.WriteLine("\n\n");
+            try
+            {
+                using (var sw = new StreamWriter("C/ffffff/test.txt"))
+                {
+                    sw.WriteLine("Hello");
+                }
+            }
+            // Put the more specific exceptions first.
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // Put the least specific exception last.
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
