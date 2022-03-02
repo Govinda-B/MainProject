fd3ebcd8443dbb8c639d02dae29d2697b6782111
Govinda B <govinda.b@thinkbridge.in>
2022-03-02 23:24:36 +0530

    Delegates, try-catch and using statements in exception handling, && and || operators, new operator

---
+++ CodingConventions/ExampleClass.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingConventions
+{
+    internal class ExampleClass
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public double Age { get; set; }
+    }
+}
